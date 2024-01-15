c8ac8967fdf078a4f57df169ef1d08f4f229af55
JayOneTheSk8 <misterjustincox@gmail.com>
2024-01-15 12:26:29 -0500

    add AuthProvider to index

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 
 import Root from './Root';
 
+import { AuthProvider } from './authContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Root />
+    <AuthProvider>
+      <Root />
+    </AuthProvider>
   </React.StrictMode>
 );
