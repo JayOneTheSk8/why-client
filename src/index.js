1154b9260a02e5c6b1d12ec46b08601edabe11e4
JayOneTheSk8 <misterjustincox@gmail.com>
2024-01-22 20:45:18 -0500

    wrap entire app in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 
 import Root from './Root';
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <Root />
+      <BrowserRouter>
+        <Root />
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
 );
