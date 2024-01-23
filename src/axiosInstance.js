4a2cd578497bd5c38e001b3cc17dca440d55c568
JayOneTheSk8 <misterjustincox@gmail.com>
2024-01-22 20:35:39 -0500

    use withCredentials for cross-site requests

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_WHY_SERVER_BASE_URL,
+  withCredentials: true,
   headers: { 'CLIENT-TOKEN': process.env.REACT_APP_CLIENT_TOKEN }
 });
