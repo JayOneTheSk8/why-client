88b311e2fe7e40220a5f0b8d19c5992f8a7e5617
JayOneTheSk8 <misterjustincox@gmail.com>
2024-01-28 18:23:11 -0500

    allow manual resizing of comment/like/repost svgs

--- src/components/Shared/RepostIcon.js
+++ src/components/Shared/RepostIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core';
 
-const RepostIcon = ({ classes, highlighted }) => {
+const RepostIcon = ({ classes, highlighted, width, height }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -10,6 +10,8 @@ const RepostIcon = ({ classes, highlighted }) => {
       enableBackground="new 18.2 25.4 39.6 25.2"
       xmlSpace="preserve"
       className={classes.repostIcon}
+      width={width || '1.5em'}
+      height={height || '1.2em'}
     >
       <path fill={highlighted ? '#00AF75' : '#848484'} d="M57,40h-6V28.25c0-0.785-0.967-1.25-1.75-1.25H35l4,4h7v9h-6l8.5,9 M26.75,49C25.967,49,25,48.535,25,47.75  V36h-6l8.5-9l8.5,9h-6l-0.065,8.963h6.442L41,49H26.75z" />
     </svg>
@@ -18,8 +20,7 @@ const RepostIcon = ({ classes, highlighted }) => {
 
 const styles = () => ({
   repostIcon: {
-    width: '1.5em',
-    height: '1.2em',
+
   },
 });
 
