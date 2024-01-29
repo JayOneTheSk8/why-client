c94c92384610c8f6011c44f1db1ffdc7c82e65ce
JayOneTheSk8 <misterjustincox@gmail.com>
2024-01-28 19:10:57 -0500

    add backend routes for comment and post data

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -13,14 +13,19 @@ export default {
   backend: {
     frontPage: '/front_page',
     frontPageFollowing: '/front_page_following',
+
     logOut: '/sign_out',
     sessions: '/sessions',
     signIn: '/sign_in',
     signUp: '/sign_up',
+
     comments: '/comments',
+    commentData: (commentId) => `/comments/${commentId}/data`,
     commentLikes: '/comment_likes',
     commentReposts: '/comment_reposts',
+
     posts: '/posts',
+    postData: (postId) => `/posts/${postId}/data`,
     postLikes: '/post_likes',
     postReposts: '/post_reposts',
   },
