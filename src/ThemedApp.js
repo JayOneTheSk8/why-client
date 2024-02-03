e8ee62369767533cc8abbb92719fbb2b0a41b989
JayOneTheSk8 <misterjustincox@gmail.com>
2024-02-03 01:42:34 -0500

    reload page when dark mode checkbox is clicked more than once - this will prevent the issue where the theme is only partially rerendered. It's really a bandage but it helps functionality and give a VERY slightly better UX when toggling it.

--- src/ThemedApp.js
+++ src/ThemedApp.js
@@ -20,20 +20,26 @@ const {
   },
 } = constants;
 
+const ogDarkModeActive = !!localStorage.getItem(DARK_MODE_ACTIVE);
+
 const ThemedApp = () => {
   const [theme, setTheme] = useState(localStorage.getItem(DARK_MODE_ACTIVE) ? darkTheme : defaultTheme);
 
   useEffect(() => {
     const handler = (event) => {
       // For some reason if this event is sent too many times the theme isn't updated.
-      // Unsure why but it works fine after ~3 clicks or so.
-      setTheme(event.detail.darkMode ? darkTheme : defaultTheme);
+      // Unsure why but it works fine after ~3 clicks. so we'll reload when the button is clicked more than once.
+      const { darkMode } = event.detail;
 
-      if (event.detail.darkMode) {
+      setTheme(darkMode ? darkTheme : defaultTheme);
+
+      if (darkMode) {
         localStorage.setItem(DARK_MODE_ACTIVE, 'true');
       } else {
         localStorage.removeItem(DARK_MODE_ACTIVE);
       }
+
+      darkMode === ogDarkModeActive && window.location.reload();
     };
 
     window.addEventListener(DARK_MODE_EVENT, handler);
