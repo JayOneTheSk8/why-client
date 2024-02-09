e3fd821f72765dcd6aab2a2ac689ae6eb9997663
JayOneTheSk8 <misterjustincox@gmail.com>
2024-02-08 23:27:29 -0500

    allow search glass to be highlighted

--- src/components/SearchPage/SearchGlassIcon.js
+++ src/components/SearchPage/SearchGlassIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core';
 
-const SearchGlassIcon = ({ classes, width, height }) => {
+const SearchGlassIcon = ({ classes, width, height, highlighted }) => {
   return (
     <svg
       className={classes.searchGlassIcon}
@@ -12,6 +12,7 @@ const SearchGlassIcon = ({ classes, width, height }) => {
       viewBox="0 0 32 32"
       version="1.1"
       xmlSpace="preserve"
+      strokeWidth={highlighted ? 3 : 1}
     >
       <g transform="matrix(1,0,0,1,-432,-48)">
         <path d="M452.626,70.009C452.64,70.025 452.655,70.04 452.67,70.056L460.254,77.639C460.644,78.029 461.277,78.029 461.668,77.639C462.058,77.249 462.058,76.615 461.668,76.225L454.084,68.641C454.068,68.625 454.052,68.61 454.035,68.595C455.679,66.629 456.669,64.096 456.669,61.334C456.669,55.079 451.59,50 445.334,50C439.079,50 434,55.079 434,61.334C434,67.59 439.079,72.669 445.334,72.669C448.111,72.669 450.655,71.668 452.626,70.009ZM445.334,52C450.486,52 454.669,56.183 454.669,61.334C454.669,66.486 450.486,70.669 445.334,70.669C440.183,70.669 436,66.486 436,61.334C436,56.183 440.183,52 445.334,52Z" />
@@ -23,6 +24,7 @@ const SearchGlassIcon = ({ classes, width, height }) => {
 const styles = theme => ({
   searchGlassIcon: {
     fill: theme.palette.primary.text,
+    stroke: theme.palette.primary.text,
   },
 });
 
